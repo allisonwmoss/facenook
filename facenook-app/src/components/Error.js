1000e299cd83a608e4c4f74314d8f27d2ddc8e25
Allison Moss <allisonwmoss@icloud.com>
2021-09-13 17:32:51 -0400

    Styling is more or less complete.

--- facenook-app/src/components/Error.js
+++ facenook-app/src/components/Error.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import styled from 'styled-components'
+import styled, { withTheme } from 'styled-components'
 
 const ErrorDiv = styled.div`
 background-color: #eb0081;
+padding: 250px 100px;
 `
 
 export default function Error(props) {
+    const LinkStyle = {
+        color: 'white',
+        textDecoration: 'underline',
+    }
     const reason = props.reason
     if (reason === "no-match") {
         return (
             <ErrorDiv>
                 <h2>Sorry, we didn't find a villager with that name.</h2>
-                <Link to="/error">
+                <Link style={LinkStyle} to="/error">
                     <h2>Click here to try again.</h2>
                 </Link>
             </ErrorDiv>
@@ -21,7 +26,7 @@ export default function Error(props) {
     return (
         <ErrorDiv>
             <h1>Uh-oh, something went wrong.</h1>
-            <Link to="/error">
+            <Link style={LinkStyle} to="/error">
                 <h2>Click here to go back home.</h2>
             </Link>
         </ErrorDiv>
--- facenook-app/src/components/VillagerDetail.js
+++ facenook-app/src/components/VillagerDetail.js
@@ -1,26 +1,32 @@
 import React, { useEffect, useState } from 'react'
 import styled, { keyframes, withTheme } from 'styled-components'
 
-const ContainerDiv = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-`
-
 const VillagerDiv = styled.div`
-margin-top: 20px;
-width: 500px;
-display: flex;
-flex-direction: column;
-align-items: center;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    padding: 5% 0%;
+    margin: 50px;
+    min-width: 850px;
+    border-radius: 10%;
 `
 
 const VillagerImg = styled.img`
-width: 75%;
-margin-top: 10%;
-margin-bottom: 10%;
-border: 5px solid white;
+    flex-grow: 1;
+    /* width: 40%;
+    height: 40%; */
+    /* max-width: 50%;
+    max-height: 50%; */
+    margin: 5%;
+    border: 50px solid white;
+`
+
+const VillagerInfoBox = styled.div`
+flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    margin-right: 5%;
 `
 
 export default function VillagerDetail(props) {
@@ -51,67 +57,79 @@ export default function VillagerDetail(props) {
             backgroundColor: cardColor,
             color: textColor,
         }
-        const boxStyle = {
-            display: 'flex',
-            flexDirection: 'column',
-            marginBottom: '10%',
-        }
         const rowStyle = {
             backgroundColor: bubbleColor,
             display: 'flex',
             flexDirection: 'row',
             justifyContent: 'space-between',
             alignItems: 'center',
-            margin: '2% 0%',
+            margin: '3% 0%',
         }
-
         const elemStyle = {
             padding: '0px 30px',
             display: 'flex',
             flexDirection: 'column',
         }
+
+        const nameStyle = {
+            backgroundColor: bubbleColor,
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-around',
+            alignItems: 'center',
+            margin: '3% 0%',
+            flexGrow: '1',
+            // fontSize: '0.8em',
+        }
         return (
             <div>
-                <ContainerDiv>
-                    <VillagerDiv
-                        style={cardStyle}
-                    >
+                {/* <ContainerDiv> */}
+                <VillagerDiv
+                    style={cardStyle}
+                >
+                    <div style={elemStyle}>
                         <VillagerImg src={villager['image_uri']} alt={villager.name['name-USen']} />
-                        <div style={boxStyle}>
-                            <div style={rowStyle}>
-                                <h1 style={elemStyle}>{villager.name['name-USen']}</h1>
-                                <h2 style={elemStyle}><em>{villager.species}</em></h2>
+                        <div style={nameStyle}>
+                            <h2 style={elemStyle}>{villager.name['name-USen']}</h2>
+                            <h2 style={elemStyle}><em>{villager.species}</em></h2>
+                        </div>
+                    </div>
+                    <VillagerInfoBox>
+                        {/* <div style={rowStyle}>
+                            <h1 style={elemStyle}>{villager.name['name-USen']}</h1>
+                            <h2 style={elemStyle}><em>{villager.species}</em></h2>
+                        </div> */}
+                        <div style={rowStyle}>
+                            <div style={elemStyle}>
+                                <h2>Birthday</h2>
+                                <h3>{villager['birthday-string']}</h3>
                             </div>
-                            <div style={rowStyle}>
-                                <div style={elemStyle}>
-                                    <h2>Birthday</h2>
-                                    <h3>{villager['birthday-string']}</h3>
-                                </div>
-                                <div style={elemStyle}>
-                                    <h2>Personality</h2>
-                                    <h3>{villager.personality}</h3>
-                                </div>
-                            </div>
-                            <div style={rowStyle}>
-                                <div style={elemStyle}>
-                                    <h2>Catchphrase</h2>
-                                    <h3>'{villager['catch-translations']['catch-USen']}'</h3>
-                                </div>
-                                <div style={elemStyle}>
-                                    <h2>Hobby</h2>
-                                    <h3>{villager.hobby}</h3>
-                                </div>
-                            </div>
-                            <div style={rowStyle}>
-                                <div style={elemStyle}>
-                                    <h4>Quote</h4>
-                                    <p><em>"{villager.saying}"</em></p>
-                                </div>
-
+                            <div style={elemStyle}>
+                                <h2>Personality</h2>
+                                <h3>{villager.personality}</h3>
                             </div>
                         </div>
-                    </VillagerDiv >
-                </ContainerDiv>
+                        <div style={rowStyle}>
+                            <div style={elemStyle}>
+                                <h2>Catchphrase</h2>
+                                <h3>'{villager['catch-translations']['catch-USen']}'</h3>
+                            </div>
+                            <div style={elemStyle}>
+                                <h2>Hobby</h2>
+                                <h3>{villager.hobby}</h3>
+                            </div>
+                        </div>
+                        <div style={rowStyle}>
+                            <div style={elemStyle}>
+                                <h2>Quote</h2>
+                                <h3><em>"{villager.saying}"</em></h3>
+                            </div>
+
+                        </div>
+                    </VillagerInfoBox>
+                    {/* </div> */}
+                </VillagerDiv >
+                {/* </ContainerDiv> */}
             </div>
         )
     }
