cebca95c52d75b7aed8ab1da785e62839ceaca6d
Allison Moss <allisonwmoss@icloud.com>
2021-09-14 11:54:18 -0400

    Still resolving Heroku issue.

--- src/index.js
+++ src/index.js
@@ -6,10 +6,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import '/Users/allisonmoss/dev/facenook/facenook-app/src/App.css'
+// import '/Users/allisonmoss/dev/facenook/src/App.css'
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom'
-import './App.css'
 
 ReactDOM.render(
   <Router>
